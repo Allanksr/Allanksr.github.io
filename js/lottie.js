505023d799277e0d086d37c206feb6c109def9f2
Allan Ribas <allanksr@gmail.com>
2020-11-09 01:19:51 -0300

    Add files via upload

---
+++ js/lottie.js
@@ -0,0 +1,9 @@
+var nova = {
+		wrapper: document.getElementsByClassName('github_icon'),
+		animType: 'svg',
+		loop: 2,
+		prerender: true,
+		autoplay: true,
+		path: './img/github_icon.json'
+	}
+lottie.loadAnimation(nova)
